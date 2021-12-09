40c88ea737ab8ed4938c86b2b408b6cf4aabeb0f
Lucas <58853488+Walife14@users.noreply.github.com>
2021-12-09 23:59:45 +0000

    User entries + clear
    Input fields now work I had to add a new input for the value, and also the clear button works, although it has to reload the page once it clears in order to get a new list done - simpler than deleting current table and creating a new one.
    
    next steps would be to add a button to delete a specific localStorage item.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // Grab elements
-const table = document.querySelector('#table')
 
+const table = document.querySelector('#table')
+const form = document.querySelector('#myForm')
+const keyInput = document.querySelector('#myKeyInput')
+const valueInput = document.querySelector('#myValueInput')
+const clearBtn = document.querySelector('#clear-btn')
 
 // functions
 
@@ -34,11 +38,63 @@ const loopStorage = () => {
 
 }
 
+const addItemToList = (key, value) => {
+    // same as last few steps in loopStorage
+    const tr = document.createElement('tr')
+    const td1 = document.createElement('td')
+    const td2 = document.createElement('td')
+    td1.textContent = key
+    td2.textContent = value
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+    table.appendChild(tr)
+}
+
+const addNewItem = (e) => {
+    e.preventDefault()
+
+    // add the input values to variables
+    const key = keyInput.value
+    const value = valueInput.value
+
+    // add the newly given values to localStorage
+    localStorage.setItem(key, value)
+    console.log(localStorage)
+
+    // run loopStorage again in order to update users list of items
+    // I've had to make a new function because re-runing loopStorage
+    // adds the previous items aswell.
+
+    addItemToList(key, value)
+
+    // clear input fields
+
+    keyInput.value = ''
+    valueInput.value = ''
+}
+
+const clearLocalStorage = () => {
+
+    // clears the localStorage
+    localStorage.clear()
+
+    // couldn't use reload on table so I force the user to reload their
+    // page.
+    location.reload()
+}
+
 
 // call functions
 
 loopStorage()
 
+// add event listeners
+
+form.addEventListener('submit', addNewItem)
+clearBtn.addEventListener('click', clearLocalStorage)
+
 // logs
+
 console.log(localStorage)
 // console.log(table)
+// console.log(form)
